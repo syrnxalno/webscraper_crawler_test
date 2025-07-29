6012222a5c93f8a619792a91aca93012a54e3d1c
Archana Krishna <150210401+syrnxalno@users.noreply.github.com>
2025-07-29 18:30:45 +0530

    Update ScraperTest.java

--- src/test/java/scrapertest/ScraperTest.java
+++ src/test/java/scrapertest/ScraperTest.java
@@ -1,1 +1,28 @@
 //unit test to be written later
+
+package com.example;
+
+import data.Product;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScraperTest {
+
+    @Test
+    public void testScrapeProductsReturnsList() {
+        String testUrl = "https://www.scrapingcourse.com/ecommerce/";
+        List<Product> products = Scraper.scrapeProducts(testUrl);
+
+        assertNotNull(products, "Product list should not be null");
+        assertFalse(products.isEmpty(), "Product list should not be empty");
+
+        for (Product p : products) {
+            assertNotNull(p.getName(), "Product name should not be null");
+            assertNotNull(p.getPrice(), "Product price should not be null");
+            assertNotNull(p.getUrl(), "Product URL should not be null");
+            assertNotNull(p.getImage(), "Product image should not be null");
+        }
+    }
+}
